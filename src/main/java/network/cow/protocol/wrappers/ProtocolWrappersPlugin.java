2256730a81b3956b9cf2ca1058edbd54c927cc0c
Benedikt Wüller <wuelleronline@gmail.com>
2021-04-01 17:11:56 +0200

    Initial commit

---
+++ src/main/java/network/cow/protocol/wrappers/ProtocolWrappersPlugin.java
@@ -0,0 +1,10 @@
+package network.cow.protocol.wrappers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Benedikt Wüller
+ */
+public class ProtocolWrappersPlugin extends JavaPlugin {
+    // Nothing to do.
+}
